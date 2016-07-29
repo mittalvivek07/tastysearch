af22d0d44a742f8b46dc811217a44b0dd75461fa
vivek <vivek>
2016-07-29 23:19:21 +0530

    Exposing over Http

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 const init = require("./init");
 const heap = require("heap");
 const k = 20;
-var server = function(){
+var express = require('express');
+var app = express();
+
+
+var engine = function(){
 	
 }
-server.prototype.start = function(){
+engine.prototype.start = function(done){
 	var startTime = new Date().getTime();
 	var self = this;
 	init("./data/foods.txt", function(docs, trees){
@@ -13,23 +17,21 @@ server.prototype.start = function(){
 		var endTime = new Date().getTime();	
 		console.log("time taken is");
 		console.log((endTime- startTime)/1000);
-		startTime = new Date().getTime();
-		console.log(self.search(["cat", "processed", "bad", "good"]));
-		var endTime = new Date().getTime();	
-		console.log(" query time taken is");
-		console.log((endTime- startTime)/1000);
+		
+		done();
 	})
 			
 }
 
 
-server.prototype.search = function(tokens){
+engine.prototype.search = function(tokens){
 	var score = {};
 	//console.log(this.docs);
 	//console.log(this.trees);
 	var startTime = new Date().getTime();
 	for(var i in tokens){
 		var token = tokens[i].toLowerCase();
+		//console.log(token);
 		for(var j in this.docs){
 			var doc = this.docs[j];
 			var tree = this.trees[j];
@@ -64,4 +66,36 @@ var comp = function(a, b){
 		return -1;
 	}
 }
-new server().start();
+
+
+
+var searchEngine = new engine();
+app.get('/search', function (req, res) {
+	var tokens;
+	try{
+		tokens = JSON.parse(req.query.tokens);
+	}catch(e){
+		 return res.status(400).send('Bad Request');
+	}
+	if(tokens === undefined){
+		return  res.status(202).end();
+	}
+	
+	startTime = new Date().getTime();
+	docs = searchEngine.search(tokens);
+	var endTime = new Date().getTime();	
+	console.log(" query time taken is");
+	console.log((endTime- startTime)/1000);
+	return res.send(docs);
+	//var docs = searchEngine.s
+  
+})
+
+var server = app.listen(8080, function () {
+	
+	searchEngine.start(function(){
+		var host = server.address().address
+		var port = server.address().port
+		console.log("Example app listening at http://%s:%s", host, port);
+	});
+})
